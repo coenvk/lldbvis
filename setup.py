18c08f6fb5e9864e56bd6f529793552eb5cee5ad
campoe <c.m.vankampen@live.nl>
2018-11-19 00:38:36 +0100

    Update setup file

--- setup.py
+++ setup.py
@@ -5,9 +5,14 @@ packages = find_packages()
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(name="lldbvis",
       version="0.0.2",
       description="A GUI application to visualize debug data.",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url="https://github.com/campoe/lldbvis",
       author="Coen van Kampen",
       author_email="c.m.vankampen14@gmail.com",
@@ -21,11 +26,13 @@ setup(name="lldbvis",
       install_requires=requirements,
       keywords=['lldbvis', 'lldb', 'python', 'debugger', 'visualization', 'qt4', 'GUI'],
       classifiers=[
-          'Environment :: GUI',
+          'Environment :: X11 Applications :: Qt',
           'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
           'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 2 :: Only',
           'Topic :: Utilities',
-          'Topic :: Debugging',
           'Topic :: Software Development :: Libraries :: Python Modules',
           'Topic :: Software Development :: User Interfaces'
       ]
